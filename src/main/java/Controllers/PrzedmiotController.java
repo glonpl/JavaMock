389c7f349d65b341660f64809449f750f7d41179
glonpl <bratajczyk@o2.pl>
2019-04-17 15:42:35 +0200

    minor fixes

--- src/main/java/Controllers/PrzedmiotController.java
+++ src/main/java/Controllers/PrzedmiotController.java
@@ -63,6 +63,6 @@ public class PrzedmiotController {
 
         return przedmiotRepository.DeletePrzedmiot(przedmiot);
     }
-    
+
 
 }
--- src/main/java/Validations/IValidation.java
+++ src/main/java/Validations/IValidation.java
@@ -2,10 +2,13 @@ package Validations;
 
 import Models.Klient;
 import Models.Przedmiot;
+import Models.Zamowienie;
 
 public interface IValidation {
     boolean KlientValid(Klient klient);
     boolean KlientNull(Klient klient);
     boolean PrzedmiotValid(Przedmiot przedmiot);
     boolean PrzedmiotNull(Przedmiot przedmiot);
+    boolean ZamowienieNull(Zamowienie zamowienie);
+    boolean Zamowienievalid(Zamowienie zamowienie);
 }
