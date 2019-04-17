c6072b397dd45a305ddc2463ace92742db54844e
glonpl <bratajczyk@o2.pl>
2019-04-17 17:17:36 +0200

    Easymocking in progress

--- src/test/java/KlientControllerEMTest.java
+++ src/test/java/KlientControllerEMTest.java
@@ -1,4 +1,53 @@
+import Controllers.KlientController;
+import Interfaces.IKlient;
+import Interfaces.IZamowienie;
+import Models.Klient;
+import Validations.IValidation;
 import org.easymock.EasyMock;
-public class KlientControllerEMTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class KlientControllerEMTest {
+    private IKlient klientRepository;
+    private IValidation validator;
+    private IZamowienie zamowienieRepository;
+    private KlientController klientController;
+
+    @BeforeEach
+    void setUp(){
+        klientRepository = EasyMock.createMock(IKlient.class);
+        validator = EasyMock.createMock(IValidation.class);
+        zamowienieRepository = EasyMock.createMock(IZamowienie.class);
+        klientController = new KlientController(klientRepository,validator, zamowienieRepository);
+    }
+    //adding
+    @Test
+    void AddClientNullThrowsException(){
+        Klient klient = new Klient(1,"Adam","Jodłowski","ajodl@o2.pl");
+        expect(validator.KlientNull(klient)).andReturn(true);
+        replay(validator);
+        assertThrows(IllegalArgumentException.class, () -> klientController.AddKlient(klient), "klient is null");
+    }
+    @Test
+    void AddClientInvalidReturnsFalse(){
+        Klient klient = new Klient(1,"Adam","Jodłowski","ajodl@o2.pl");
+        expect(validator.KlientNull(klient)).andReturn(false);
+        expect(validator.KlientValid(klient)).andReturn(false);
+        replay(validator);
+        assertFalse(klientController.AddKlient(klient));
+    }
+    @Test
+    void AddClientProperlyReturnsTrue(){
+        Klient klient = new Klient(1,"Adam","Jodłowski","ajodl@o2.pl");
+        expect(validator.KlientNull(klient)).andReturn(false);
+        expect(validator.KlientValid(klient)).andReturn(true);
+        replay(validator);
+        expect(klientRepository.AddKlient(klient)).andReturn(true);
+        replay(klientRepository);
+        assertSame(true,klientController.AddKlient(klient));
+    }
 }
--- src/test/java/ZamowienieControllerWrittenTest.java
+++ src/test/java/ZamowienieControllerWrittenTest.java
@@ -2,6 +2,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ZamowienieControllerWrittenTest {
-    @BeforeEach
-    @Test
+ //   @BeforeEach
+   // @Test
 }
